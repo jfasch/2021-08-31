3365e98d1cc7ac265e96e5b9e3f3ff155eed39d8
Joerg Faschingbauer <jf@faschingbauer.co.at>
2021-09-01 11:38:46 +0200

    greet.py, optimized for readability

--- live-hacking/greet.py
+++ live-hacking/greet.py
@@ -1,18 +1,29 @@
+import sys
+
 name = input('Name: ')
 sex = input('Sex: ')
 hour = int(input('Hour: '))
 
-if hour <= 9:
+# if hour in range(0, 10):    # <<<<< does not perform so well (linear search)
+# if 0 <= hour and hour <= 9: # <<<<< unreadable but equivalent
+if 0 <= hour <= 9:            # <<<<< best fit for our purpose
     greeting = 'Good morning, '
-elif hour > 9 and hour <= 17:
+elif 10 <= hour <= 17:
     greeting = 'Good day, '
-elif hour > 17:
+elif 18 <= hour <= 23:
     greeting = 'Good evening, '
+else:
+    print('error')
+    sys.exit(1)
 
-if sex == 'm':
+# if sex == 'm' or sex == 'M':
+if sex in ('m', 'M'):
     greeting += 'Mr. '
-elif sex == 'f':
+elif sex in ('f', 'F'):
     greeting += 'Mrs. '
+else:
+    print('error')
+    sys.exit(1)
 
 greeting += name
 
